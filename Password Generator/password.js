9b3e51f545b436ecbdb93ad21b83655a355d9f47
Shubham Kumar <99100178+AditiyaSharma@users.noreply.github.com>
2022-08-02 20:13:45 +0530

    Add files via upload

---
+++ Password Generator/password.js
@@ -0,0 +1,48 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+generateEl.addEventListener('click', () => {
+    resultEl.innerText = generatePassword(lowercaseEl.checked, uppercaseEl.checked, numbersEl.checked, symbolsEl.checked, +lengthEl.value);
+});
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+    // Doesn't have a selected type
+    if (typesCount === 0) {
+        return '';
+    }
+    // create a loop
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+}
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber() {
+    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*.'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
